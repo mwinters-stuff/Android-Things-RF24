33fda260282052f87f2f44dd0d1483447ffc0c90
Mathew Winters <mathew@winters.org.nz>
2017-01-31 19:44:41 +1300

    Add empty test.

---
+++ app/src/test/java/com/example/androidthings/rf24/rf24/RF24Test.java
@@ -0,0 +1,344 @@
+package com.example.androidthings.rf24.rf24;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mathew on 31/01/17.
+ * Copyright 2017 Mathew Winters
+ */
+public class RF24Test {
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void ce() throws Exception {
+
+  }
+
+  @Test
+  public void beginTransaction() throws Exception {
+
+  }
+
+  @Test
+  public void readRegister() throws Exception {
+
+  }
+
+  @Test
+  public void readRegister1() throws Exception {
+
+  }
+
+  @Test
+  public void writeRegister() throws Exception {
+
+  }
+
+  @Test
+  public void writeRegister1() throws Exception {
+
+  }
+
+  @Test
+  public void writePayload() throws Exception {
+
+  }
+
+  @Test
+  public void readPayload() throws Exception {
+
+  }
+
+  @Test
+  public void flushRx() throws Exception {
+
+  }
+
+  @Test
+  public void flushTx() throws Exception {
+
+  }
+
+  @Test
+  public void transfer() throws Exception {
+
+  }
+
+  @Test
+  public void spiTrans() throws Exception {
+
+  }
+
+  @Test
+  public void getStatus() throws Exception {
+
+  }
+
+  @Test
+  public void printStatus() throws Exception {
+
+  }
+
+  @Test
+  public void printObserveTx() throws Exception {
+
+  }
+
+  @Test
+  public void printByteRegister() throws Exception {
+
+  }
+
+  @Test
+  public void printAddressRegister() throws Exception {
+
+  }
+
+  @Test
+  public void min() throws Exception {
+
+  }
+
+  @Test
+  public void setChannel() throws Exception {
+
+  }
+
+  @Test
+  public void getPayloadSize() throws Exception {
+
+  }
+
+  @Test
+  public void setPayloadSize() throws Exception {
+
+  }
+
+  @Test
+  public void printDetails() throws Exception {
+
+  }
+
+  @Test
+  public void _BV() throws Exception {
+
+  }
+
+  @Test
+  public void begin() throws Exception {
+
+  }
+
+  @Test
+  public void startListening() throws Exception {
+
+  }
+
+  @Test
+  public void stopListening() throws Exception {
+
+  }
+
+  @Test
+  public void powerDown() throws Exception {
+
+  }
+
+  @Test
+  public void powerUp() throws Exception {
+
+  }
+
+  @Test
+  public void millis() throws Exception {
+
+  }
+
+  @Test
+  public void write() throws Exception {
+
+  }
+
+  @Test
+  public void write1() throws Exception {
+
+  }
+
+  @Test
+  public void writeBlocking() throws Exception {
+
+  }
+
+  @Test
+  public void writeFast() throws Exception {
+
+  }
+
+  @Test
+  public void writeFast1() throws Exception {
+
+  }
+
+  @Test
+  public void startWrite() throws Exception {
+
+  }
+
+  @Test
+  public void rxFifoFull() throws Exception {
+
+  }
+
+  @Test
+  public void txStandBy() throws Exception {
+
+  }
+
+  @Test
+  public void txStandBy1() throws Exception {
+
+  }
+
+  @Test
+  public void maskIRQ() throws Exception {
+
+  }
+
+  @Test
+  public void getDynamicPayloadSize() throws Exception {
+
+  }
+
+  @Test
+  public void available() throws Exception {
+
+  }
+
+  @Test
+  public void available1() throws Exception {
+
+  }
+
+  @Test
+  public void read() throws Exception {
+
+  }
+
+  @Test
+  public void whatHappened() throws Exception {
+
+  }
+
+  @Test
+  public void openWritingPipe() throws Exception {
+
+  }
+
+  @Test
+  public void openWritingPipe1() throws Exception {
+
+  }
+
+  @Test
+  public void openReadingPipe() throws Exception {
+
+  }
+
+  @Test
+  public void setAddressWidth() throws Exception {
+
+  }
+
+  @Test
+  public void openReadingPipe1() throws Exception {
+
+  }
+
+  @Test
+  public void closeReadingPipe() throws Exception {
+
+  }
+
+  @Test
+  public void enableDynamicPayloads() throws Exception {
+
+  }
+
+  @Test
+  public void enableAckPayload() throws Exception {
+
+  }
+
+  @Test
+  public void enableDynamicAck() throws Exception {
+
+  }
+
+  @Test
+  public void writeAckPayload() throws Exception {
+
+  }
+
+  @Test
+  public void isAckPayloadAvailable() throws Exception {
+
+  }
+
+  @Test
+  public void setAutoAck() throws Exception {
+
+  }
+
+  @Test
+  public void setAutoAck1() throws Exception {
+
+  }
+
+  @Test
+  public void testCarrier() throws Exception {
+
+  }
+
+  @Test
+  public void testRPD() throws Exception {
+
+  }
+
+  @Test
+  public void getPALevel() throws Exception {
+
+  }
+
+  @Test
+  public void setPALevel() throws Exception {
+
+  }
+
+  @Test
+  public void setCRCLength() throws Exception {
+
+  }
+
+  @Test
+  public void disableCRC() throws Exception {
+
+  }
+
+  @Test
+  public void setRetries() throws Exception {
+
+  }
+
+}
