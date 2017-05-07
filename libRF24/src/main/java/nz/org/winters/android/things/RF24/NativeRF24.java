2583d58a99f882f5c152d3747178ac6a0c35a233
Mathew Winters <mathew@winters.org.nz>
2017-05-07 20:13:43 +1200

    Add interfaces for mocking without loading native libraries.

--- libRF24/src/main/java/nz/org/winters/android/things/RF24/NativeRF24.java
+++ libRF24/src/main/java/nz/org/winters/android/things/RF24/NativeRF24.java
@@ -12,7 +12,7 @@ import java.lang.annotation.RetentionPolicy;
  */
 
 @SuppressWarnings({"unused", "WeakerAccess"})
-public class NativeRF24 implements Closeable{
+public class NativeRF24 implements NativeRF24Interface, Closeable{
   public static final int RF24_PA_MIN = 0;
   public static final int RF24_PA_LOW = 1;
   public static final int RF24_PA_HIGH = 2;
@@ -57,114 +57,166 @@ public class NativeRF24 implements Closeable{
     closeDevices();
   }
 
+  @Override
   public native void closeDevices();
 
+  @Override
   public native void init(int cePin, int spiFrequency, int spiBus);
 
+  @Override
   public native void begin();
 
+  @Override
   public native boolean available();
 
+  @Override
   public native int availablePipe();
 
+  @Override
   public native void enableAckPayload();
 
+  @Override
   public native void enableDynamicPayloads();
 
+  @Override
   public native void disableDynamicPayloads();
 
+  @Override
   public native void enableDynamicAck();
 
+  @Override
   public native void setAutoAck(int pipe, boolean enable);
 
+  @Override
   public native void setAutoAck(boolean enable);
 
+  @Override
   public native void writeAckPayload(int pipe, byte[] buf, int len);
 
+  @Override
   public native boolean isAckPayloadAvailable();
 
+  @Override
   public native boolean txStandBy(long timeout, boolean startTx);
 
+  @Override
   public native boolean txStandBy();
 
+  @Override
   public native boolean writeBlocking(byte[] buffer, int len, long timeout);
 
+  @Override
   public native boolean writeFastEx(byte[] buffer, int len, boolean multicast);
 
+  @Override
   public native boolean writeFast(byte[] buffer, int len);
 
+  @Override
   public native boolean write(byte[] buffer, int len);
 
+  @Override
   public native boolean writeEx(byte[] buffer, int len, boolean multicast);
 
 
+  @Override
   public native void powerUp();
 
+  @Override
   public native void powerDown();
 
+  @Override
   public native boolean rxFifoFull();
 
 
+  @Override
   public native boolean isPVariant();
 
+  @Override
   public native int getDynamicPayloadSize();
 
+  @Override
   public native int getPayloadSize();
 
+  @Override
   public native void setPayloadSize(int size);
 
+  @Override
   public native int getChannel();
 
+  @Override
   public native void setChannel(int channel);
 
+  @Override
   public native void setRetries(int delay, int count);
 
+  @Override
   public native boolean testRPD();
 
+  @Override
   public native boolean testCarrier();
 
+  @Override
   public native int flush_tx();
 
+  @Override
   public native void reUseTx();
 
-
+  @Override
   public native void setAddressWidth(int a_width);
 
+  @Override
   public native void openReadingPipeStr(int pipe, String address);
 
+  @Override
   public native void openReadingPipe(int pipe, long address);
 
+  @Override
   public native void closeReadingPipe(int pipe);
 
+  @Override
   public native void openWritingPipe(long address);
 
+  @Override
   public native void openWritingPipeStr(String address);
 
+  @Override
   public native void startWrite(byte[] buf, int len, boolean multicast);
 
+  @Override
   public native void startFastWrite(byte[] buf, int len, boolean multicast, boolean startTx);
 
 
+  @Override
   public native void startListening();
 
+  @Override
   public native void stopListening();
 
+  @Override
   public native byte[] read(int length);
 
+  @Override
   public native void disableCRC();
 
+  @Override
   public native int getCRCLength();
 
+  @Override
   public native void setCRCLength(int length);
 
+  @Override
   public native int getDataRate();
 
+  @Override
   public native void setDataRate(int speed);
 
+  @Override
   public native int getPALevel();
 
+  @Override
   public native void setPALevel(int level);
 
+  @Override
   public native void printDetails();
 
 }
--- libRF24/src/main/java/nz/org/winters/android/things/RF24/NativeRF24Network.java
+++ libRF24/src/main/java/nz/org/winters/android/things/RF24/NativeRF24Network.java
@@ -6,7 +6,7 @@ import android.support.annotation.IntRange;
  * @author MWinters
  */
 @SuppressWarnings("unused")
-public class NativeRF24Network {
+public class NativeRF24Network implements NativeRF24NetworkInterface {
   static {
     System.loadLibrary("RF24");
   }
@@ -32,46 +32,67 @@ public class NativeRF24Network {
     init();
   }
 
+  @Override
   public native void init();
 
+  @Override
   public native void begin(int nodeAddress);
 
+  @Override
   public native int update();
 
+  @Override
   public native boolean available();
 
+  @Override
   public native int peek(RF24NetworkHeader header);
 
+  @Override
   public native int read(RF24NetworkHeader header, byte[] message, int maxLen);
 
+  @Override
   public native boolean write(RF24NetworkHeader header, byte[] message, int maxLen);
 
+  @Override
   public native void multicastLevel(@IntRange(from = 1, to = 6) int level);
 
+  @Override
   public native void setMulticastRelay(boolean enable);
 
+  @Override
   public native void setTxTimeout(int timeout);
 
+  @Override
   public native int getTxTimeout();
 
+  @Override
   public native void setRouteTimeout(int timeout);
 
+  @Override
   public native int getRouteTimeout();
 
+  @Override
   public native boolean multicast(RF24NetworkHeader header, byte[] message, int maxLen, int level);
 
+  @Override
   public native boolean write(RF24NetworkHeader header, byte[] message, int maxLen, int writeDirect);
 
+  @Override
   public native int parent();
 
+  @Override
   public native int addressOfPipe(int node, int pipeNo);
 
+  @Override
   public native boolean isValidAddress(int node);
 
+  @Override
   public native void begin(int channel, int node_address);
 
+  @Override
   public native void setNetworkFlags(int flags);
 
+  @Override
   public native int getNetworkFlags();
 
 
